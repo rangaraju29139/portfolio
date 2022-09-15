054f2716c99f76a164a41cd4e871cda0cea2ba42
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-16 01:43:33 +0530

    added journey component with initial contents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import Banner from "./components/Banner";
+import Journey from "./components/Journey";
 
 function App() {
   return (
     <>
       <Navigation />
       <Banner />
+      <Journey />
     </>
   );
 }
---
+++ src/components/Journey.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/journey.css";
+
+function Journey() {
+  return (
+    <>
+      <div className=".grid-container journey-container">
+        <h1 className="grid-item">This is the journey timeline compnent</h1>
+      </div>
+    </>
+  );
+}
+export default Journey;
